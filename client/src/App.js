515aa9f3b891916334047cf8f7f47960e83ea8d1
Alan Kamei <gaipuilankmei@gmail.com>
2024-11-04 22:06:52 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi there we will be working for CATCH ministry project from now on!</h1>
+    </div>
+  );
+}
+
+export default App;
